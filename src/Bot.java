01dd3456732e36fcdb2dd30514b323f6be756cd3
elusive935 <alenka.fml@mail.ru>
2019-09-08 18:42:50 +0300

    Обработка команд

--- src/Bot.java
+++ src/Bot.java
@@ -1,14 +1,21 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.logging.Logger;
+
 public class Bot extends TelegramLongPollingBot {
+    private Logger log = Logger.getLogger(Bot.class.getSimpleName());
 
     Bot() {
     }
 
     public void onUpdateReceived(Update update) {
         String message = update.getMessage().getText();
-        System.out.println(message);
+        if ("/govno".equals(message)) {
+            System.out.println("Вот говно!");
+        } else {
+            System.out.println(message);
+        }
     }
 
     public String getBotUsername() {
